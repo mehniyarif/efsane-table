8436848bef1d54c67ad6d07b0c502a6aa06f1c50
Arif Mehniyar <arif.mehniyar@gmail.com>
2022-03-13 01:51:14 +0300

    version 1.0.6

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -7,12 +7,26 @@ export default {
     input: 'src/components/efsane-table/index.vue', // Path relative to package.json
     external: [
         'vue',
-        '@babel/runtime/regenerator'
+        '@babel/runtime/regenerator',
+        '@babel/runtime/helpers/esm/toConsumableArray',
+        '@babel/runtime/helpers/esm/asyncToGenerator',
+        '@babel/runtime/helpers/esm/typeof',
+        '@babel/runtime/helpers/esm/slicedToArray',
+        '@babel/runtime/helpers/esm/classCallCheck',
+        '@babel/runtime/helpers/esm/createClass',
+        '@babel/runtime/helpers/esm/defineProperty',
       ],
     output: {
         globals: {
           'vue': 'Vue',
-          '@babel/runtime/regenerator': '_regeneratorRuntime'
+          '@babel/runtime/regenerator': '_regeneratorRuntime',
+          '@babel/runtime/helpers/esm/toConsumableArray': '_toConsumableArray',
+          '@babel/runtime/helpers/esm/asyncToGenerator': '_asyncToGenerator',
+          '@babel/runtime/helpers/esm/typeof': '_typeof',
+          '@babel/runtime/helpers/esm/slicedToArray': '_slicedToArray',
+          '@babel/runtime/helpers/esm/classCallCheck': '_classCallCheck',
+          '@babel/runtime/helpers/esm/createClass': '_createClass',
+          '@babel/runtime/helpers/esm/defineProperty': '_defineProperty',
         },
         name: 'EfsaneTable',
         exports: 'auto',
@@ -32,7 +46,10 @@ export default {
             "@babel/plugin-transform-async-to-generator",
             ["@babel/plugin-transform-runtime", { useESModules: true }]
           ],
-          presets: ["@babel/preset-env"],
+          presets: ["@babel/preset-env",
+            ["@vue/app", {
+            "useBuiltIns": "entry"
+          }]],
           exclude: "node_modules/**"
         }),
         commonjs(),
