bd79c514d34ac1dedd85ed1b04e6e1e43a7c8c46
Arif Mehniyar <arif.mehniyar@gmail.com>
2022-03-19 19:43:03 +0300

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">English Premier Leage</router-link> |
+      <router-link to="/">England Premier Leage</router-link> |
       <router-link to="/documentation">Documentation</router-link>
     </div>
     <router-view/>
